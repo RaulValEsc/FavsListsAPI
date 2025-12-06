c4dbca3952b094a81c0520267c64a3d0ea311e4f
RaulValEsc <raulvalesc@gmail.com>
2025-12-06 19:27:52 +0100

    Initialize Spring Boot project

---
+++ src/main/java/com/raulvalesc/favslistsapi/FavsListsApiApplication.java
@@ -0,0 +1,13 @@
+package com.raulvalesc.favslistsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavsListsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavsListsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raulvalesc/favslistsapi/FavsListsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulvalesc.favslistsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavsListsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
