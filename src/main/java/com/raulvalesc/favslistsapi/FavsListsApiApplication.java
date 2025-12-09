8c8ce1884874d4fa5055c12ab933ba2de3a01493
RaulValEsc <raulvalesc@gmail.com>
2025-12-09 22:00:33 +0100

    Add Injectable custom anotation

--- src/main/java/com/raulvalesc/favslistsapi/FavsListsApiApplication.java
+++ src/main/java/com/raulvalesc/favslistsapi/FavsListsApiApplication.java
@@ -1,9 +1,15 @@
 package com.raulvalesc.favslistsapi;
 
+import com.raulvalesc.favslistsapi.shared.domain.injectable.Injectable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+        includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Injectable.class)
+)
 public class FavsListsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FavsListsApiApplication.class, args);
---
+++ src/main/java/com/raulvalesc/favslistsapi/shared/domain/injectable/Injectable.java
@@ -0,0 +1,8 @@
+package com.raulvalesc.favslistsapi.shared.domain.injectable;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface Injectable {}
